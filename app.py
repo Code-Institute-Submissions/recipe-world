6a558b436db4bfa0e9ba8d04141843b83adc4c05
Peter Krivan <krivan.peter@outlook.com>
2019-02-02 17:13:09 +0000

    bootstrap navbar added, desserts and breakfast menu connected to database

--- app.py
+++ app.py
@@ -11,10 +11,19 @@ app.config["MONGO_URI"] = "mongodb://krivan.peter:jel123szo@ds149122.mlab.com:49
 mongo = PyMongo(app)
 
 @app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
 @app.route("/get_desserts")
 def get_desserts():
     return render_template("desserts.html", 
     desserts=mongo.db.desserts.find())
+    
+@app.route("/get_breakfasts")
+def get_breakfasts():
+    return render_template("breakfast.html", 
+    breakfasts=mongo.db.breakfasts.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
