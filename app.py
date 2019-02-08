3c14f86682f703eff3805bad0f0e8daee5d35360
Peter Krivan <krivan.peter@outlook.com>
2019-02-08 08:30:59 +0000

    link are placed into navbar

--- app.py
+++ app.py
@@ -15,16 +15,20 @@ mongo = PyMongo(app)
 def index():
     return render_template("index.html")
 
-@app.route("/get_desserts")
+@app.route("/desserts")
 def get_desserts():
-    return render_template("desserts.html", 
+    return render_template("dessert.html", 
     desserts=mongo.db.desserts.find())
     
-@app.route("/get_breakfasts")
+@app.route("/breakfasts")
 def get_breakfasts():
     return render_template("breakfast.html", 
     breakfasts=mongo.db.breakfasts.find())
 
+@app.route("/mains")
+def get_lunchs():
+    return render_template("main.html", 
+    mains=mongo.db.mains.find())
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
