a499025f8a1202b1599fa1e9e15d1bb691057296
Peter Krivan <krivan.peter@outlook.com>
2019-02-15 14:23:35 +0000

    sign up works, check if username and email exist, if not they are inserted to database

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, session, json, jsonify
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.secret_key = "ran1dom2string3"
 
 app.config["MONGO_DBNAME"] = "recipeworlddb"
 app.config["MONGO_URI"] = "mongodb://krivan.peter:jel123szo@ds149122.mlab.com:49122/recipeworlddb"
@@ -13,13 +14,13 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html",
-    foods=mongo.db.foods.find())
+    foods=mongo.db.foods.find()
+    return render_template("index.html", foods=foods)
 
 @app.route("/breakfasts")
 def get_breakfasts():
-    return render_template("index.html", 
-    foods=mongo.db.foods.find({"category_name": "breakfast"}), title="Breakfasts")
+    foods=mongo.db.foods.find({"category_name": "breakfast"})
+    return render_template("index.html", foods=foods, title="Breakfasts")
 
 @app.route("/mains")
 def get_mains():
@@ -31,10 +32,29 @@ def get_desserts():
     return render_template("index.html", 
     foods=mongo.db.foods.find({"category_name": "dessert"}), title="Desserts")
     
-@app.route('/<food_name>')
+@app.route("/<food_name>")
 def get_food(food_name):
     return render_template("food.html", 
     food=mongo.db.foods.find_one({"name": food_name}))
+
+@app.route("/sign_up", methods=['POST'])
+def sign_up():
+    exist = ""
+    username =  request.form["username"];
+    email = request.form["email"];
+    if mongo.db.users.find({"username": username}).count() >= 1 and mongo.db.users.find({"email": email}).count() == 0:
+        exist = "username_exists"
+        return exist
+    elif mongo.db.users.find({"email": email}).count() >= 1 and mongo.db.users.find({"username": username}).count() == 0:
+        exist = "email_exists"
+        return exist
+    elif mongo.db.users.find({"email": email}).count() >= 1 and mongo.db.users.find({"username": username}).count() >= 1:
+        exist = "email_and_username_exist"
+        return exist
+    else:
+        mongo.db.users.insert({ "username": username , "email": email, "favorites": "" } )
+        foods=mongo.db.foods.find()
+        return render_template("index.html", foods=foods)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
---
+++ static/js/signUp.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$('#signUpForm').on('submit', function(event) {
+		$.ajax({
+			data : {
+				username : $('#inputUsername').val(),
+				email : $('#inputEmail').val()
+			},
+			type : 'POST',
+			url : '/sign_up'
+		})
+		.done(function(exist) {
+			if (exist == "username_exists"){
+				$('#usernameErrorAlert').show();
+				$('#emailErrorAlert').hide();
+			}
+			else if(exist == "email_exists") {
+				$('#emailErrorAlert').show();
+				$('#usernameErrorAlert').hide();
+			}
+			else if(exist == "email_and_username_exist") {
+				$('#emailErrorAlert').show();
+				$('#usernameErrorAlert').show();
+			}
+			else{
+				$('#usernameErrorAlert').hide();
+				$('#emailErrorAlert').hide();
+			}
+		});
+		event.preventDefault();
+	});
+
+});
