d6183bfe9dd0c7a0cb3ecb7478dc0d1cf477a3da
Peter Krivan <krivan.peter@outlook.com>
2019-02-20 11:47:33 +0000

    for method user can add new input line with button

--- static/js/recipe.js
+++ static/js/recipe.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
     var max_fields = 10; //maximum input boxes allowed
-    var wrapper = $(".input_fields_wrap"); //Fields wrapper
-    var add_button = $(".add_field_button"); //Add button ID
+    var ingredients_wrapper = $(".ingredients_input_fields_wrap"); //Fields wrapper
+    var add_ingrediments_button = $(".add_ingrediments_field_button"); //Add button ID
+    
+    var method_wrapper = $(".method_input_fields_wrap"); //Fields wrapper
+    var add_method_button = $(".add_method_field_button"); //Add button ID
 
     var x = 1; //initlal text box count
-    $(add_button).click(function(e) { //on add input button click
+    $(add_ingrediments_button).click(function(e) { //on add input button click
         e.preventDefault();
         if (x < max_fields) { //max input box allowed
             x++; //text box increment
-            $(wrapper).append(
+            $(ingredients_wrapper).append(
                 '<div class="row">' +
                 '<div class="offset-2 col-sm-6">' +
                 '<input type="text" class="form-control" id="ingredients">' +
@@ -20,7 +23,29 @@ $(document).ready(function() {
         }
     });
 
-    $(wrapper).on("click", ".remove_field", function(e) { //user click on remove field
+    $(ingredients_wrapper).on("click", ".remove_field", function(e) { //user click on remove field
+        e.preventDefault();
+        $(this).parent('div').parent('div').remove();
+        x--;
+    })
+    
+    $(add_method_button).click(function(e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(method_wrapper).append(
+                '<div class="row">' +
+                '<div class="offset-2 col-sm-6">' +
+                '<input type="text" class="form-control" id="method">' +
+                '</div>' +
+                '<div class="col-sm-2">' +
+                '<button class="btn edit_recipe_btn remove_field"><i class="material-icons">do_not_disturb_on</i></button>' +
+                '</div>' +
+                '</div>');
+        }
+    });
+
+    $(method_wrapper).on("click", ".remove_field", function(e) { //user click on remove field
         e.preventDefault();
         $(this).parent('div').parent('div').remove();
         x--;
